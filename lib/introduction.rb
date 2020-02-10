e1f764816712f894a1fbc5ce7c6c0d6c5a222c3b
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-10 17:24:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
-def introduction(name)
-  puts "Hi, my name is #{name}.\n"
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
-introduction("Dan")
+introduction("Dan", "Ember.js")
